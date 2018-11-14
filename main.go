8ba06f7e2c1ead1beb0a18da8ecd8fd680d7e9b3
Matt Beaney <41631170+x1um1n@users.noreply.github.com>
2018-11-14 09:39:17 +0000

    Fix copy/paste fail

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ type Field struct {
 }
 
 type Action struct {
-	Type	   *string   `json:"type"`
-	Text	   *string   `json:"text"`
-	Url	   *string   `json:"url"`
-	Style	   *string   `json:"style"`
+	Type	string   `json:"type"`
+	Text	string   `json:"text"`
+	Url 	string   `json:"url"`
+	Style 	string   `json:"style"`
 }
 
 type Attachment struct {
